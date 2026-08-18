acf1c42f505c3feb790844728a2a44ac141f152e
Affan Suhendar <affansuhendar1@gmail.com>
2026-08-18 10:04:02 +0700

    Push

--- src/components/ShareAndLikeBar.tsx
+++ src/components/ShareAndLikeBar.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faWhatsapp, faFacebookF } from '@fortawesome/free-brands-svg-icons';
 
 interface ShareAndLikeBarProps {
   contentType: 'news' | 'announcements' | 'program' | 'newsletter' | 'kampanye' | 'gallery' | string;
@@ -204,9 +204,7 @@ export default function ShareAndLikeBar({
             className="relative group w-12 h-12 rounded-2xl bg-[#ea4c89] hover:bg-[#e1306c] text-white flex items-center justify-center shadow-md hover:scale-105 transition-all duration-200 cursor-pointer"
             title="Bagikan ke Instagram"
           >
-            <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24">
-              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-            </svg>
+            <FontAwesomeIcon icon={faInstagram} className="w-[28px] h-[28px] text-white" />
             {copiedInstagram && (
               <span className="absolute -top-12 left-1/2 -translate-x-1/2 bg-[#006400] text-white text-[11px] font-bold px-2.5 py-1 rounded-lg shadow-lg whitespace-nowrap z-20 animate-bounce">
                 Tautan disalin!
@@ -221,9 +219,7 @@ export default function ShareAndLikeBar({
             className="w-12 h-12 rounded-2xl bg-[#25D366] hover:bg-[#20bd5a] text-white flex items-center justify-center shadow-md hover:scale-105 transition-all duration-200 cursor-pointer"
             title="Bagikan ke WhatsApp"
           >
-            <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24">
-              <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981z" />
-            </svg>
+            <FontAwesomeIcon icon={faWhatsapp} className="w-[28px] h-[28px] text-white" />
           </button>
 
           {/* Facebook Button */}
@@ -233,9 +229,7 @@ export default function ShareAndLikeBar({
             className="w-12 h-12 rounded-2xl bg-[#1877F2] hover:bg-[#1565d8] text-white flex items-center justify-center shadow-md hover:scale-105 transition-all duration-200 cursor-pointer"
             title="Bagikan ke Facebook"
           >
-            <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24">
-              <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-            </svg>
+            <FontAwesomeIcon icon={faFacebookF} className="w-[25px] h-[25px] text-white" />
           </button>
 
           {/* Copy Link Button with Speech-Bubble Tooltip */}
@@ -258,7 +252,10 @@ export default function ShareAndLikeBar({
               className="w-12 h-12 rounded-2xl bg-[#e5e7eb] hover:bg-gray-300 text-gray-700 flex items-center justify-center shadow-xs hover:scale-105 transition-all duration-200 cursor-pointer"
               title="Salin Tautan"
             >
-              <FontAwesomeIcon icon={faLink} className="w-5 h-5 text-gray-600" />
+              <svg className="w-[28px] h-[28px] text-[#374151] fill-current" viewBox="0 0 24 24">
+                <path d="M13.59 11.59a3 3 0 0 0-4.24 0l-4.25 4.24a3 3 0 0 0 4.25 4.25l1.41-1.42a1 1 0 0 1 1.42 1.42l-1.42 1.41a5 5 0 0 1-7.07-7.07l4.24-4.24a5 5 0 0 1 7.07 0 1 1 0 0 1-1.41 1.41z" />
+                <path d="M10.41 12.41a3 3 0 0 0 4.24 0l4.25-4.24a3 3 0 0 0-4.25-4.25l-1.41 1.42a1 1 0 0 1-1.42-1.42l1.42-1.41a5 5 0 0 1 7.07 7.07l-4.24 4.24a5 5 0 0 1-7.07 0 1 1 0 0 1 1.41-1.41z" />
+              </svg>
             </button>
           </div>
         </div>
